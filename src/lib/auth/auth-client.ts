d6581bbc00de38f13375a6ab5691c53f3c4f3b35
larrybwosi <larrybwosi@gmail.com>
2026-03-28 17:26:11 +0300

    Aligning the schema to remove the errors

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,9 +1,10 @@
-"use client"
+'use client';
 
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3001",
-})
+  baseURL: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3001',
+  // plugins: [jwtClient()],
+});
 
-export const { signIn, signOut, signUp, useSession } = authClient
+export const { signIn, signOut, signUp, useSession } = authClient;
