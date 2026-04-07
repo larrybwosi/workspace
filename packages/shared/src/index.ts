af5199cdf252a95c79de1beda88e2b90d261f3cb
larrybwosi <larrybwosi@gmail.com>
2026-04-07 18:11:09 +0300

    Corrected the relation errors

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,5 +3,6 @@ export * from './agora-config';
 export * from './asset-utils';
 export * from './auth/client';
 export * from './hooks/use-mobile';
+export * from './hooks/use-callstore';
 export * from './utils/upload-utils';
 export * from './utils/date-utils';
