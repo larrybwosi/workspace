b5958dbd1146bb289d969dce066254c3d7be150e
larrybwosi <larrybwosi@gmail.com>
2026-07-07 20:59:47 +0300

    clear

--- packages/shared/src/agora-config.server.ts
+++ packages/shared/src/agora-config.server.ts
@@ -3,7 +3,7 @@ import { validateEnv } from './env';
 const env = validateEnv();
 
 export const agoraServerConfig = {
-  appId: env.NEXT_PUBLIC_AGORA_APP_ID!,
+  appId: env.AGORA_APP_ID!,
   appCertificate: env.AGORA_APP_CERTIFICATE!,
 };
 
